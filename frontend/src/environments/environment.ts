cbe8ca476bcdeeb9546e7c4c8d55de14801bfc87
Mohammad Fareed <mohammad.fareed@joshsoftware.com>
2024-11-22 05:10:40 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl : 'https://walletplus.in/portal/',
+  apiUrl : 'https://walletplus.in/',
   // apiUrl: 'http://localhost/w2/backend/',
 };
 
